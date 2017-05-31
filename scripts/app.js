6e53d97e4dd62e6b5767c6d043c7589b170bf791
RyanBacastow <washingtonirving2017@gmail.com>
2017-05-31 18:53:37 -0500

    First Commit

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.controller('myController', function ($scope) {
+    console.log("Controller Works");
+});
+
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    //$locationProvider.html5Mode(true);
+    $routeProvider.when("/", {
+        templateUrl: "templates/main.html"
+    }).when("/about", {
+        templateUrl: "templates/about.html"
+    }).when("/contact", {
+        templateUrl: "templates/contact.html",
+    }).when("/back-end", {
+        templateUrl: "templates/back.html",
+    }).when("/data-analysis", {
+        templateUrl: "templates/data-analysis",
+    }).when("/front-end", {
+        templateUrl: "templates/front-end.html",
+    }).when("/all-projects", {
+        templateUrl: "templates/all-projects",
+        controller: 'myController'
+    });
+}]);
+
+
+app.directive("navBar", function () {
+    return {
+        restrict: "E",
+        templateUrl: "/templates/nav-bar.html"
+    }
+});
